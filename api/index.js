3ee4b8bdacfbe59056ccbce5be2ce3e3227d6560
JoshGoold <joshgoold2@hotmail.com>
2024-10-17 23:31:35 -0400

    diabled websocket support in production

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(express.json());
 //sets strict origin of calls to be from local host port
 app.use(
   cors({
-    origin: ["http://localhost:3500", "http://172.18.0.1:3500", "https://jginc.org"],
+    origin: ["http://localhost:3500", "http://172.18.0.1:3500", "https://jginc.org", "http://139.177.195.151:3500"],
     credentials: true,
   })
 );
